0c294726cb9671179af6757b4f85af2f80e83ad8
iportoles <iportoles@H1102.insmollerussa.edu>
2018-03-12 10:56:50 +0100

    “Versió 0.1 del projecte m8daw2molle"

---
+++ m8daw2molle/src/m8daw2molle/M8daw2molle.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package m8daw2molle;
+
+/**
+ *
+ * @author iportoles
+ */
+public class M8daw2molle {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       // sdjkgsdikghsdhg
+    }
+    
+}
